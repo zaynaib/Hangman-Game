ac106fd5338599065d64f4fff6b5327594b2998d
Zaynaib <zaynaibg@gmail.com>
2017-09-22 22:40:19 -0500

    Cleaned up indentation and add comments to my code

--- assets/javascript/game4.js
+++ assets/javascript/game4.js
@@ -1,7 +1,4 @@
-
-
-
-//variables
+//Variables
 var wins = 0;
 var numGuessRemain =10;
 var lettersAlreadyGuessed = [];
@@ -9,127 +6,93 @@ var numberGuessesHTML = document.getElementById("numGuessElm");
 var lettersAlreadyGuessedHTML = document.getElementById("guessLetterElm");
 var wordisplayHTML = document.getElementById("wordisplay");
 
+// Creates an array that lists out all of the options.
+//this does not work with the same letter in different indices
+var hangmanChoices = ["mario", "luigi","browser","peach","toad","yoshi","daisy","waluigi", "bowser jr","koopalings"];
 
- // Creates an array that lists out all of the options.
- //this does not work with the same letter in different indices
-    var hangmanChoices = ["kanye", "cardib","tink","twista","madonna"];
-
-    //create a random generator for the hangman word
+//create a random generator for the hangman word
    
-    function hangmanGenerator(arr){
-        return hangmanChoices[Math.floor(Math.random() * arr.length)];
+function hangmanGenerator(arr){
+  return hangmanChoices[Math.floor(Math.random() * arr.length)];
+};
 
-    };
-    var hangmanWord = hangmanGenerator(hangmanChoices);
-
-     /* var hangmanPlacementholders = [];
-      for(var i = 0; i< hangmanWord.length; i++){
-        hangmanPlacementholders.push("_"); 
-        wordisplayHTML.textContent = "Current Word: " + hangmanPlacementholders.join(" ");
-      }
-      */
-
-      function placeGenerator(str){
-        var myArr = [];
-        for(var i = 0; i< str.length; i++){
-        myArr.push("_"); 
-        wordisplayHTML.textContent = "Current Word: " + myArr.join(" ");
-      }
-        return myArr;
-            };
-
-      var hangmanPlacementholders = placeGenerator(hangmanWord);
-      console.log(hangmanPlacementholders);
+var hangmanWord = hangmanGenerator(hangmanChoices);
 
 
-      
- 
-    //console.log(hangmanWord);
-    //console.log(hangmanPlacementholders);
-    
-    
+function placeGenerator(str){
+  var myArr = [];
+  for(var i = 0; i< str.length; i++){
+    myArr.push("_"); 
+    wordisplayHTML.textContent = "Current Word: " + myArr.join(" ");
+  }
+  return myArr;
+};
 
 
+var hangmanPlacementholders = placeGenerator(hangmanWord);
 
+document.onkeyup=function(event) {
+  //gets the key that the user presses
+  var keyPressed = event.key;
 
-//user press enter to play game function
+  //gets the index of the letter of computer chosen hangman word
+  var charIndex = hangmanWord.indexOf(keyPressed);
 
+  //if the user has no more guess then they lose. hahahaha!
+  if(numGuessRemain === 0){
+    alert("You lose");
+    startOver();
+  }
 
-    document.onkeyup=function(event) {
-        //gets the key from the user
-      var keyPressed = event.key;
-        //gets the index of the hangmanword
-      var charIndex = hangmanWord.indexOf(keyPressed);
+  //check to see if the letter is not inside the word 
+  if(charIndex === -1){
+    lettersAlreadyGuessed.push(keyPressed); 
+    numGuessRemain=numGuessRemain-1; //decrement number of guesses
+    alert("Try Again! You are wack."); //pop alert try again 
+    numberGuessesHTML.textContent = "Number of Guesses Remaining: " + numGuessRemain;
+    lettersAlreadyGuessedHTML.textContent = "Letters Already Guessed: " + lettersAlreadyGuessed.join(" ");
+    }
 
-      if(numGuessRemain === 0){
-        alert("You lose");
-        startOver();
-
-        /*alert("Press Enter to start over");
-          if(keyPressed === "Enter"){
-             startOver();
-          }
-          */
-      }
-
-        //check to see if the letter is inside the word
-        if(charIndex === -1){
+    else{
+      lettersAlreadyGuessed.push(keyPressed);
+      numGuessRemain = numGuessRemain-1;
+      for(var i=0;i<hangmanPlacementholders.length;i++){
+        if(hangmanWord[i] === keyPressed){
+          hangmanPlacementholders[i] = keyPressed;
+          wordisplayHTML.textContent = "Current Word: " + hangmanPlacementholders.join(" "); 
+          lettersAlreadyGuessedHTML.textContent = "Letters Already Guessed: " + lettersAlreadyGuessed.join(" ");
+          }//end of nested if loop inside of for loop
             
-            lettersAlreadyGuessed.push(keyPressed); 
-            
-            numGuessRemain=numGuessRemain-1; //decrement number of guesses
-            alert("Try Again! You are wack."); //pop alert try again 
-            numberGuessesHTML.textContent = "Number of Guesses Remaining: " + numGuessRemain;
-            lettersAlreadyGuessedHTML.textContent = "Letters Already Guessed: " + lettersAlreadyGuessed.join(" ");
-        }
-
-
-        else{
-          lettersAlreadyGuessed.push(keyPressed);
-          numGuessRemain = numGuessRemain-1;
-          for(var i=0;i<hangmanPlacementholders.length;i++){
-
-            if(hangmanWord[i] === keyPressed){
-              hangmanPlacementholders[i] = keyPressed;
-              wordisplayHTML.textContent = "Current Word: " + hangmanPlacementholders.join(" "); 
-              lettersAlreadyGuessedHTML.textContent = "Letters Already Guessed: " + lettersAlreadyGuessed.join(" ");
-            }
-            
-          }
-          numberGuessesHTML.textContent = "Number of Guesses Remaining: " + numGuessRemain;
-          console.log(hangmanPlacementholders);
+        }//end of for loop
+        numberGuessesHTML.textContent = "Number of Guesses Remaining: " + numGuessRemain;
          
-        }
+        }//end of else statement
 
+        //joins the word that the user is guessing
         var hangmanCompare = hangmanPlacementholders.join("");
-        console.log(hangmanCompare);
-
-       //if(hangmanPlacementholders.indexOf("_") === -1 ){
-         if(hangmanCompare === hangmanWord){
-            wins+=1;
-            document.getElementById('winElm').textContent='Wins:'+wins;   
-            hangmanCompare = "blank";
-            console.log(hangmanCompare);
-            startOver();
+        
+        //checks to see if the user's guess is exactly equal to the computer generated word
+        if(hangmanCompare === hangmanWord){
+          wins+=1; //increment win
+          document.getElementById('winElm').textContent='Wins:'+wins; //show win up to the DOM
+          hangmanCompare = "blank"; //reset the user's guess to a black slate so it won't meet the condition
+          startOver(); //start the game over
 
          }
          
 
        
-
+      //This function resets all the variables to start the game over
       function startOver(){
         keyPressed = "";
         numGuessRemain =10;
         lettersAlreadyGuessed = [];
         myPlaceholders =[];
         hangmanWord = hangmanGenerator(hangmanChoices);
-        hangmanPlacementholders = placeGenerator(hangmanWord);
+        hangmanPlacementholders = placeGenerator(hangmanWord); 
         numberGuessesHTML.textContent = "Number of Guesses Remaining: ";
         lettersAlreadyGuessedHTML.textContent = "Letters Already Guessed: ";
         wordisplayHTML.textContent = "Current Word: ";
-        console.log("startOver");
-        console.log(hangmanWord);
-
         
       }
 
@@ -139,17 +102,7 @@ var wordisplayHTML = document.getElementById("wordisplay");
       };
 
       
-/*
-      function startOver(){
-        numGuessRemain =10;
-        lettersAlreadyGuessed = [];
-        hangmanWord = hangmanGenerator(hangmanChoices);
-        numberGuessesHTML.textContent = "Number of Guesses Remaining: ";
-        lettersAlreadyGuessedHTML.textContent = "Letters Already Guessed: ";
-        wordisplayHTML.textContent = "Current Word: ";
-        
-      }
-      */
+
 
      
 
