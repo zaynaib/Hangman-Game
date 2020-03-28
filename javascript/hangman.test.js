fd75c4786054154082155201f21af5f59d94b1f3
Zaynaib Giwa <zaynaibg@gmail.com>
2020-03-28 17:05:44 -0500

    first working test

--- javascript/hangman.test.js
+++ javascript/hangman.test.js
@@ -1,23 +1,10 @@
 //import Model from './model';
 const Model = require('./model');
 
-jest.mock('./model.js');
-
 it('works', () => {
-    let mario = new Model();
-    expect(mario).toBe(hangmanChoices)
-})
-// jest.mock('./model');
-
-// beforeEach(() => {
-//     // Clear all instances and calls to constructor and all methods:
-//     Model.mockClear();
-// });
-
-// it('We can check if the consumer called the class constructor', () => {
-//     const marioData = new Model();
-//     expect(marioData).toHaveBeenCalledTimes(1);
-// });
+	let mario = new Model();
+	expect(mario).toBeTruthy();
+});
 
 // let mario = new Model();
-// console.log(mario)
+// console.log(mario);
