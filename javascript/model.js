cb42cc780f32c18a18d022e74321df8d8362dc4e
Zaynaib Giwa <zaynaibg@gmail.com>
2020-04-03 20:43:06 -0500

    fix file paths in index.html file

--- javascript/model.js
+++ javascript/model.js
@@ -1,15 +1,23 @@
- class Model {
-     constructor() {
-         //List of hangman guesses for the game
-         this.hangmanChoices = ["mario", "lugi", "browser", "peach", "toad", "yoshi", "daisy", "waluigi", "bowser", "koopalings"]
-     }
+class Model {
+	constructor() {
+		//List of hangman guesses for the game
+		this.hangmanChoices = [
+			'mario',
+			'lugi',
+			'browser',
+			'peach',
+			'toad',
+			'yoshi',
+			'daisy',
+			'waluigi',
+			'bowser',
+			'koopalings',
+		];
+	}
 
-     get firstChoice() {
-         return this.hangmanChoices[0]
-     }
- }
+	get firstChoice() {
+		return this.hangmanChoices[0];
+	}
+}
 
- // let mario = new Model();
- // console.log(mario)
-
- module.exports = Model;
+module.exports = Model;
