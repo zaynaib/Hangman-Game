1ce58ce765bd1844671f7f796dc4f29eafa1f8ac
Zaynaib Giwa <zaynaibg@gmail.com>
2020-04-06 18:28:38 -0500

    create getters and setters for model

--- javascript/model.js
+++ javascript/model.js
@@ -1,4 +1,5 @@
 //What is the data of this app?
+//MVC resource https://www.taniarascia.com/javascript-mvc-todo-app/
 /*
  - the words for the hangman game
  - how many wins the player have
@@ -25,10 +26,35 @@ class Model {
 
 		this.numGuessesRemain = 10;
 		this.numWins = 0;
+		this.numLoss = 0;
 	}
 
-	get firstChoice() {
-		return this.hangmanChoices[0];
+	get hangmanChoices() {
+		return this.hangmanChoices;
+	}
+
+	get numGuessesRemain() {
+		return this.numGuessesRemain;
+	}
+
+	get numWins() {
+		return this.numWins;
+	}
+
+	get numLoss() {
+		return this.numLoss;
+	}
+
+	set numGuessesRemain(numberOfGuesses) {
+		this.numGuessesRemain = numberOfGuesses;
+	}
+
+	set numWins(numberOfWins) {
+		this.numWins = numberOfWins;
+	}
+
+	set numLoss(numberOfLoss) {
+		this.numLoss = numberOfLoss;
 	}
 }
 
